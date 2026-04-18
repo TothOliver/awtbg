56fff8d17efa7e22871880f63916eb9c4e4fce92
Johan <johan.wisell52@gmail.com>
2026-04-18 14:42:33 +0200

    Added Quit option to death scene

--- Scripts/deathScene.gd
+++ Scripts/deathScene.gd
@@ -53,3 +53,7 @@ func _on_restart_pressed():
 	GameStats.innocent_robots_killed = 0
 	GameStats.bad_robots_terminated = 0 # Reset this!
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
