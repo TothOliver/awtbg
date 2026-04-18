8030d0cbdf3d104fd7fb74de4ee6159e1e4f0455
TothOliver <olivertoth.in@gmail.com>
2026-04-18 13:57:39 +0200

    bug fix label

--- Scripts/ChatBubble.gd
+++ Scripts/ChatBubble.gd
@@ -3,4 +3,6 @@ extends Control
 @onready var label = $Label
 
 func set_message(text: String):
+	if label == null:
+		return
 	label.text = text
--- Scripts/ChatManager.gd
+++ Scripts/ChatManager.gd
@@ -4,6 +4,8 @@ extends Control
 var bubble_scene = preload("res://Scenes/ChatBubble.tscn")
 	
 func add_message(text: String):
+	if text.is_empty():
+		return
 	var bubble = bubble_scene.instantiate()
 	dialogue_panel.add_child(bubble)
 	bubble.set_message(text)
